c393d86d7d0b945f016ee71bf1993476a46de6ef
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2018-12-19 21:12:09 -0500

    Create unverified-sender.ps1
    Base upload

---
+++ unverified-sender.ps1
@@ -0,0 +1,23 @@
+$ruleName = "Unverified Sender - No SPF Validation"
+$Username = "EMAIL"
+$Password = ConvertTo-SecureString ‘PASSWORD’ -AsPlainText -Force
+$Session = New-PSSession -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
+    -ConfigurationName Microsoft.Exchange -Credential $credentials `
+    -Authentication Basic -AllowRedirection
+Import-PSSession $Session -AllowClobber
+$rule = Get-TransportRule | Where-Object {$_.Identity -contains $ruleName}
+if (!$rule) {
+    Write-Host "Rule not found, creating rule" -ForegroundColor Green
+    New-TransportRule -Name $ruleName -Priority 1 -HeaderContainsMessageHeader "Authentication-Results"`
+    -HeaderContainsWords "spf=TempError","spf=PermError","spf=None","spf=Neutral","spf=SoftFail","spf=Fail"`
+    -PrependSubject "[Unverified Sender] "
+}
+else {
+    Write-Host "Rule found, updating rule" -ForegroundColor Green
+    Set-TransportRule -Identity $ruleName
+    -Priority 1
+    -HeaderContainsMessageHeader "Authentication-Results"`
+    -HeaderContainsWords "spf=TempError","spf=PermError","spf=None","spf=Neutral","spf=SoftFail","spf=Fail"`
+    -PrependSubject "[Unverified Sender] "
+}
+Remove-PSSession $Session
