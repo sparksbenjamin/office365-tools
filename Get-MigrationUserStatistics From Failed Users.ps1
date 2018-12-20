815e620bb96b82675a8ee99dc4c8069db576d27a
tomturpin <tom@turpin.us>
2018-12-20 09:03:24 -0500

    Add files via upload
    Assuming you are connected to o365 with Admin Creds, this script will step through non-synced accounts and provide error messages from the attempted sync.

---
+++ Get-MigrationUserStatistics From Failed Users.ps1
@@ -0,0 +1,35 @@
+﻿#$O365credential = Get-Credential
+#$O365Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $O365credential -Authentication Basic -AllowRedirection
+#Import-PSSession $O365Session -DisableNameChecking -AllowClobber
+
+$failed = Get-MigrationUser -ResultSize unlimited | where {$_.status -eq "Failed"};
+
+if ( $failed -eq $null ) {
+    Write-Host No Accounts have Failed at this time...
+} else {
+    $Activity = "Number of Accounts in ERROR: "+$failed.count;
+    $i = 0;
+    foreach ($user in $failed) { 
+        $i++;
+        Write-Progress -Activity $Activity -status "Processing: $i" -percentComplete ($i / $failed.count*100);
+        Get-MigrationUserStatistics -Identity $user.Identity| Select BatchID, identity, state, status, *count*;
+    };
+    $Activity;
+};
+
+$synching = Get-MigrationUser -ResultSize unlimited | where {$_.status -ne 'synced' -and $_.status -ne 'failed' -and $_.status -ne 'Completed'};
+
+if ( $synching -eq $null ) {
+    Write-Host No Accounts Synching at this time...
+} else {
+    $Activity = "Accounts that are synching"+$synching.Count;
+    $i = 0;
+    foreach ($sync in $synching) { 
+        $i++;
+        Write-Progress -Activity $Activity -status "Processing: $i" -percentComplete ($i / $synching.count*100);
+        Get-MigrationUserStatistics -Identity $sync.Identity| Select BatchID, identity, state, status, *count*;
+        $Activity;
+     };
+};
+
+#Remove-PSSession $O365Session
