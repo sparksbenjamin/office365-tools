ad2a031b1dd4a498dbdeca5697e21bfb15a06222
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2021-06-11 23:24:36 -0400

    Update spoofed_domain.ps1

--- transport_rules/spoofed_domain.ps1
+++ transport_rules/spoofed_domain.ps1
@@ -1,4 +1,5 @@
-$ruleName = "Unverified Sender - No SPF Validation - Sending Domain"
+$ruleName = "Unverified Sender - No SPF Validation - SELF SPOOFING"
+$DOMAIN = @('EXAMPLE.COM')
 $Username = "EMAIL"
 $Password = ConvertTo-SecureString ‘PASSWORD’ -AsPlainText -Force
 $Session = New-PSSession -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
@@ -7,19 +8,16 @@ $Session = New-PSSession -ConnectionUri https://outlook.office365.com/powershell
 Import-PSSession $Session -AllowClobber
 $rule = Get-TransportRule | Where-Object {$_.Identity -contains $ruleName}
 if (!$rule) {
-    Write-Host "Rule not found, creating rule" -ForegroundColor Green
-    New-TransportRule -Name $ruleName -Priority 1 -HeaderContainsMessageHeader "Authentication-Results"`
+    Write-Host "Rule found, updating rule" -ForegroundColor Green
+    Set-TransportRule -Identity $ruleName -Priority 1  -Quarantine $True -SenderDomainIs $DOMAIN -RecipientDomainIs $DOMAIN -HeaderContainsMessageHeader "Authentication-Results"`
     -HeaderContainsWords "spf=TempError","spf=PermError","spf=None","spf=Neutral","spf=SoftFail","spf=Fail"`
-    -PrependSubject "[Unverified Sender] "
-    -Quarantine True
     -Comments "Rule Created by https://github.com/sparksbenjamin/base-secure-exchange"
     
 }
 else {
     Write-Host "Rule found, updating rule" -ForegroundColor Green
-    Set-TransportRule -Identity $ruleName -Priority 1 -HeaderContainsMessageHeader "Authentication-Results"`
+    Set-TransportRule -Identity $ruleName -Priority 1  -Quarantine $True -SenderDomainIs $DOMAIN -RecipientDomainIs $DOMAIN -HeaderContainsMessageHeader "Authentication-Results"`
     -HeaderContainsWords "spf=TempError","spf=PermError","spf=None","spf=Neutral","spf=SoftFail","spf=Fail"`
-    -Comments "Rule Created by https://github.com/sparksbenjamin/base-secure-exchange"`
-    -PrependSubject "[Unverified Sender] "
+    -Comments "Rule Created by https://github.com/sparksbenjamin/base-secure-exchange"
 }
 Remove-PSSession $Session
